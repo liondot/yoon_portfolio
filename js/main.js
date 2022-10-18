74174bc57a5d4d5f15382f9ad7f34da2b2932ed8
liondot <51257576+liondot@users.noreply.github.com>
2022-10-18 12:01:21 +0900

    작업중

--- js/main.js
+++ js/main.js
@@ -12,13 +12,17 @@ $(function () {
 	let scrollTxtBottom;
 
 	// my skill 
-	let scrollBody = $('.my_skill');
-	let titText = scrollBody.find('.intro_txt');
-	let maskLeft = scrollBody.find('.left_mask');
-	let maskRight = scrollBody.find('.right_mask');
-	let bgImage = scrollBody.find('.bg_img');
-	let bgImageTwo = scrollBody.find('.bg_img2')
-	let endingContent = scrollBody.find('.ending_txt');
+	let mySkill = $('.my_skill');
+	let skills = $('.skills');
+	// let valCon = $('.value_con')
+
+	let titText = mySkill.find('.intro_txt');
+	let maskLeft = mySkill.find('.left_mask');
+	let maskRight = mySkill.find('.right_mask');
+	let bgImage = mySkill.find('.bg_img');
+	let bgImageTwo = mySkill.find('.bg_img2');
+	let endingContent = mySkill.find('.ending_txt');
+
 
 	// 메인타이틀 라인  
 	let webLine = $('.web_line');
@@ -30,20 +34,11 @@ $(function () {
 	let toyContainer = toyClone.find('.container');
 	let ctTitle = toyClone.find('.ct_title');
 
-	let cloneBodyScrollHeight;
+	let cloneBodymySkillHeight;
 	let cloneBodyOffsetTop;
 	let cloneBodyScrollTop;
 	let cloneBodyRealHeight;
 	let isMobile;
-
-	/*리사이즈, 스크롤할때 변해야 할 값들*/
-	let scrollHeight;
-	let sectionOffsetTop;
-	let sectionScrollTop;
-	let scrollRealHeight;
-	let scrollPercent;
-	let percent;
-
 
 	let toyCloneHeight;
 	let toyCloneOffsetTop;
@@ -52,24 +47,55 @@ $(function () {
 	let toyCloneRealHeight;
 	let toyPer;
 
+	let skillCode = $('.skill_code');
+	let codeSkillText = skillCode.find('.skill_txt');
+	let codeSkillBar = skillCode.find('.skill_bar');
+	let skillDesign = $('.skill_design');
+	let designSkillText = skillDesign.find('.skill_txt');
+	let designSkillBar = skillDesign.find('.skill_bar');
+
+
+	let mySkillHeight;
+	let mySkillRealHeight;
+	let mySkillOffsetTop;
+	let valueThisTop;
+	let valuePercent;
+	let parallaxSpeed = 1200;
+
+	let valConThisTop
+	let valConPercent
+
+	let maxNum;
 
 
 	function setProperty() {
-		// section 2 scroll_text 위치 값   
 		winScrollTop = $(window).scrollTop();
+
+		// section 2 scroll_text 위치 값   
 		scrollTxtTop = scrollTxt.offset().top;
 		scrollTxtBottom = scrollTxtTop + scrollTxt.height();
 		textInOut()
-		
-		// my skill & project 위치값
-		scrollHeight = scrollBody.height();
-		sectionOffsetTop = scrollBody.offset().top;
-		scrollRealHeight = (scrollHeight - $(window).height());
-		winScrollTop = $(window).scrollTop();
-		sectionScrollTop = winScrollTop - sectionOffsetTop;
 
-		scrollPercent = sectionScrollTop / scrollRealHeight;
-		percent = scrollPercent * 100;
+		// my skill & project 위치값
+		mySkillHeight = mySkill.height();
+		mySkillRealHeight = (mySkillHeight - $(window).height())
+		skillScrollTop = winScrollTop - mySkillOffsetTop;
+		skillScrollPercent = skillScrollTop / mySkillRealHeight;
+		percent = skillScrollPercent * 100;
+
+		mySkillOffsetTop = mySkill.offset().top;
+		valueThisTop = winScrollTop - mySkillOffsetTop;
+		valuePercent = valueThisTop / parallaxSpeed * 100;
+
+		let maxNumber = 100;
+		maxNum = Math.floor(Math.min(maxNumber, (valueThisTop / mySkillRealHeight) * 100));
+
+
+		var valueHeightPercent = (valueThisTop / mySkillRealHeight) * 100
+		var valueTextPercent = Math.round(valueHeightPercent)
+
+		// console.log('maxNum', maxNum)
+		render(valueTextPercent, valueHeightPercent);
 
 		toyCloneOffsetTop = toyClone.offset().top;
 		toyCloneHeight = toyClone.height();
@@ -78,11 +104,73 @@ $(function () {
 		toyClonePercent = toyCloneScrollTop / toyCloneRealHeight;
 		toyPer = toyClonePercent * 100;
 
-
-		console.log('toyCloneOffsetTop', toyCloneOffsetTop)
-		console.log('toyPer', toyPer)
-
 	}
+
+
+
+	function render(valueTextPercent, valueHeightPercent) {
+		if (maxNum > 100) { //100이 넘어갈 때 더이상 값이 올리가지 않음, 100으로 최대값 맞춤
+
+			// codeSkillText.text("100 " + '%')
+			// designSkillText.text("100 " + '%')
+
+			// codeSkillBar.css({ 
+			// 	width: "40" + '%'
+			// });
+			// designSkillBar.css({ 
+			// 	width: "40" + '%'
+			// });
+
+
+		} else if (maxNum < 100) {
+			//index : 0-html , 1-css, 2-sass, 3-js, 4-jquery, 5-react
+			codeSkillText.eq(0).text(Math.floor(valueTextPercent / 1.18) + '%'); //html skill
+			codeSkillText.eq(1).text(Math.floor(valueTextPercent / 1.15) + '%'); //css skill 
+			codeSkillText.eq(2).text(Math.floor(valueTextPercent / 1.4) + '%'); //sass skill
+			codeSkillText.eq(3).text(Math.floor(valueTextPercent / 1.9) + '%'); //js skill 
+			codeSkillText.eq(4).text(Math.floor(valueTextPercent / 1.8) + '%'); //jquery skill
+			codeSkillText.eq(5).text(Math.floor(valueTextPercent / 1.6) + '%'); //react skill 
+
+			//index: 0-photoshop, 1-illustrator, 2-figma
+			designSkillText.eq(0).text(Math.floor(valueTextPercent / 1.2) + '%'); //photoshop skill
+			designSkillText.eq(1).text(Math.floor(valueTextPercent / 1.25) + '%'); //illust skill 
+			designSkillText.eq(2).text(Math.floor(valueTextPercent / 2) + '%'); //figma skill 
+
+
+			// skillbar render 
+			codeSkillBar.eq(0).css({ //html skill
+				width: valueHeightPercent / 1.4 + '%'
+			});
+			codeSkillBar.eq(1).css({ //css skill
+				width: valueHeightPercent / 1.6 + '%'
+			});
+			codeSkillBar.eq(2).css({ //sass skill
+				width: valueHeightPercent / 1.7 + '%'
+			});
+			codeSkillBar.eq(3).css({ //js skill
+				width: valueHeightPercent / 2 + '%'
+			});
+			codeSkillBar.eq(4).css({ //jquery skill
+				width: valueHeightPercent / 1.9 + '%'
+			});
+			codeSkillBar.eq(5).css({ //react skill
+				width: valueHeightPercent / 1.8 + '%'
+			});
+
+			designSkillBar.eq(0).css({ //phosothop skill
+				width: valueHeightPercent / 1.1 + '%'
+			});
+			designSkillBar.eq(1).css({ //illust skill
+				width: valueHeightPercent / 1.2 + '%'
+			});
+			designSkillBar.eq(2).css({ //figma skill
+				width: valueHeightPercent / 2 + '%'
+			});
+		}
+
+
+
+	};
 
 	function moveFunc() {
 		setProperty()
@@ -93,7 +181,7 @@ $(function () {
 		} else {}
 
 		// toyclone project 모바일버전 pc
-		if(isMobile) {
+		if (isMobile) {
 			contentInMobile();
 		} else {
 			contentIn();
@@ -105,35 +193,37 @@ $(function () {
 		var gap = 1;
 
 		allText.each(function (index, arr) {
-			$(arr).attr( 
+			$(arr).attr(
 				'style',
 				'--progress:' + (Math.max(0, dis - (index * gap))) + ''
 			)
 		})
 	}
 
-
+	// my skill 좌우 사라지는 배경 위치 애니메이션 
 	function motionRender() {
-		let maskStartValue = 50;
+		let maskStartValue = 60;
 		let maskEndValue = -10;
-		let zoomValue = 1.5;
+		let zoomValue = 2;
 		let zoomOutValue = 1;
-		let maskVal = Math.max(maskEndValue, maskStartValue - (scrollPercent * maskStartValue));
-		let scaleVal = Math.max(zoomOutValue, zoomValue - (scrollPercent * zoomValue));
+		let maskVal = Math.max(maskEndValue, maskStartValue - (skillScrollPercent * maskStartValue));
+		let scaleVal = Math.max(zoomOutValue, zoomValue - (skillScrollPercent * zoomValue));
 
+		// my skill 화면 열림 maskLeft, maskRight
 		maskLeft
 			.css({
-				width: maskVal + '%'
+				width: (maskVal / 1.3) + '%'
 			});
 		maskRight
 			.css({
-				width: maskVal + '%'
+				width: (maskVal / 1.3) + '%'
 			});
 		bgImage
 			.css({
 				'transform': 'scale(' + scaleVal + ')'
 			});
 
+		// my skill 처음, 마지막 텍스트 
 		if (percent > 0.3) {
 			titText.addClass('active');
 
@@ -141,14 +231,16 @@ $(function () {
 			titText.removeClass('active');
 		}
 
-		if (percent >= 40) {
+		if (percent >= 10) {
 			endingContent.addClass('active');
+			skills.addClass('active')
 		} else {
 			endingContent.removeClass('active');
+			skills.removeClass('active')
 		}
 
 
-	
+
 
 		// 스롤이 my_project 섹션에 도착하면 라인 애니메이션 실행 
 		if (percent > 105) {
@@ -157,13 +249,13 @@ $(function () {
 			webLine.eq(0).removeClass('active')
 		}
 		//스크롤이 toy_clone 섹션에 위치하면 라인 애니메이션 실행 
-		if (percent > 130) {
+		if (percent > 135) {
 			webLine.eq(1).addClass('active')
 		} else if (percent < 139 || percent > 200) {
 			webLine.eq(1).removeClass('active')
 		}
 
-		
+
 
 		// my_project 스크롤 위치에 도착하면 컨텐츠 애니메이션 실행 
 		if (percent > 107 && percent < 200) {
@@ -184,38 +276,33 @@ $(function () {
 
 	};
 
-
+	// clone toy 프로젝트 스크롤 시 텍스트 이미지 맞춤 변경 
 	function contentIn() {
 		var deviceImg = $('.device_fix .slide_wrap figure');
 		var imgWidth = deviceImg.width();
-		console.log('toyPer', toyPer)
-		console.log('deviceImg', deviceImg)
-		console.log('imgWidth', imgWidth)
 
-		
-		if(toyPer >= 0 && toyPer < 20) {
+		if (toyPer >= 0 && toyPer < 20) {
 			imageChange(imgWidth * 0);
 			$('#clone_toy_project .text_box .txt01').addClass('active');
-
 		}
 
-		if(toyPer >= 20 && toyPer < 54) {
+		if (toyPer >= 20 && toyPer < 54) {
 			imageChange(imgWidth * 1);
 			$('#clone_toy_project .text_box .txt02').addClass('active');
 		}
 
-		if(toyPer >= 54 && toyPer < 85) {
+		if (toyPer >= 54 && toyPer < 85) {
 			imageChange(imgWidth * 2);
 			$('#clone_toy_project .text_box .txt03').addClass('active');
 		}
 
-		if(toyPer >= 85 ) {
+		if (toyPer >= 85) {
 			imageChange(imgWidth * 3);
-			console.log(imgWidth*3)
+			console.log(imgWidth * 3)
 			$('#clone_toy_project .text_box .txt04').addClass('active');
 		}
 
-		if(toyPer < 0 ) {
+		if (toyPer < 0) {
 			$('#clone_toy_project .text_box .txt01').removeClass('active');
 			$('#clone_toy_project .text_box .txt02').removeClass('active');
 			$('#clone_toy_project .text_box .txt03').removeClass('active');
@@ -223,32 +310,33 @@ $(function () {
 		}
 	};
 
+	// 모바일  clone toy 프로젝트 스크롤 시 텍스트 이미지 맞춤 변경  
 	function contentInMobile() {
 
 		var deviceImg = $('.device_fix .slide_wrap figure');
 		var imgWidth = deviceImg.width();
-	
+
 		console.log('imgWidth', imgWidth)
 
-		if(toyPer >= 5 && toyPer < 25){
+		if (toyPer >= 5 && toyPer < 25) {
 			imageChange(imgWidth * 0);
 			$('#clone_toy_project .text_box p').removeClass('active');
 			$('#clone_toy_project .text_box .txt01').addClass('active');
 		}
 
-		if(toyPer >= 25 && toyPer < 45) {
+		if (toyPer >= 25 && toyPer < 45) {
 			imageChange(imgWidth * 1);
 			$('#clone_toy_project .text_box p').removeClass('active');
 			$('#clone_toy_project .text_box .txt02').addClass('active');
 		}
 
-		if(toyPer >= 45 && toyPer < 65) {
+		if (toyPer >= 45 && toyPer < 65) {
 			imageChange(imgWidth * 2);
 			$('#clone_toy_project .text_box p').removeClass('active');
 			$('#clone_toy_project .text_box .txt03').addClass('active');
 		}
 
-		if(toyPer >= 65 && toyPer <= 85) {
+		if (toyPer >= 65 && toyPer <= 85) {
 			imageChange(imgWidth * 3);
 			$('#clone_toy_project .text_box p').removeClass('active');
 			$('#clone_toy_project .text_box .txt04').addClass('active');
@@ -256,12 +344,12 @@ $(function () {
 		}
 
 
-		if(toyPer > 85) {
+		if (toyPer > 85) {
 			imageChange(imgWidth * 3);
 			$('#clone_toy_project .text_box p').removeClass('active');
 		}
 
-		if(toyPer < 0) {
+		if (toyPer < 0) {
 			(imgWidth * 0);
 			$('#clone_toy_project .text_box p').removeClass('active');
 		}
@@ -274,6 +362,19 @@ $(function () {
 			transform: 'translateX(' + -moveX + 'px)'
 		})
 	}
+
+	// contact me 버튼 클릭시 해당 영역으로 이동
+	contactForm = $('#contact_form')
+	contactBtn = $('#contact_btn')
+	socialBtn = $('#social_btn')
+	contactContainer = contactForm.find('.container')
+
+	socialBtn.click(function () {
+		contactContainer.addClass('change_mod')
+	})
+	contactBtn.click(function () {
+		contactContainer.removeClass('change_mod')
+	})
 
 
 
@@ -293,50 +394,3 @@ $(function () {
 
 	init()
 });
-
-
-
-
-// contact me 
-contactForm = $('#contact_form')
-contactBtn = $('#contact_btn')
-socialBtn = $('#social_btn')
-contactContainer = contactForm.find('.container')
-
-socialBtn.click(function(){
-	contactContainer.addClass('change_mod')
-})
-contactBtn.click(function(){
-	contactContainer.removeClass('change_mod')
-})
-
-
-// final_notice 
-
-
-
-// /************** 숫자를 바꿔 보며 원을 그려보세요:) 잘못넣으면 오류가 생기니 따옴표 지우지말고 조심조심 바꾸세요 ^.^; *******/
-// function canvasCircle(){
-// $('#canvas').circleDraw({ //정방향 그리기
-// 	drawDistance:{
-// 		start : 0, //선 시작위치(deg도)  12시 : 0, 3시 :90, 6시: 180, 9시: 270
-// 		end : 80, //선이그려지는거리(%퍼센트): 100%(맥시멈) * end가 0이면 그려지지 않음.
-// 		line : 'round' //라인끝모양 종류: butt|round|square
-// 	},
-// 	speed:1,//그리는 속도 낮을수록 빨리 그림
-// 	reverse:false, //true 일경우 역방향 진행, false일경우 정방향 진행
-// 	backCircle:{
-// 		radius: 200,//원 크기
-// 		color1: '#b1b1b1',//그라데이션컬리1
-// 		color2: '#b1b1b1',//그라데이션컬러2
-// 		lineWidth : 70 //보더넓이
-// 	},
-// 	frontCircle:{
-// 		radius: 200,
-// 		color1: '#090979',
-// 		color2: '#00d4ff',
-// 		lineWidth : 70
-// 	}
-// });
-// }
-
